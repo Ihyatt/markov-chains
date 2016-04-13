e3e1f1e378007b767963bed2ccef0b3dfa7de796
Hackbright Student <info@hackbrightacademy.com>
2016-04-12 17:29:44 -0700

    resolved key error

--- markov.py
+++ markov.py
@@ -3,7 +3,6 @@ from random import choice
 
 def open_and_read_file(file_path):
     """Takes file path as string; returns text as string.
-
     Takes a string that is a file path, opens the file, and turns
     the file's contents as one string of text.
     """
@@ -13,13 +12,10 @@ def open_and_read_file(file_path):
 
 def make_chains(text_string):
     """Takes input text as string; returns _dictionary_ of markov chains.
-
     A chain will be a key that consists of a tuple of (word1, word2)
     and the value would be a list of the word(s) that follow those two
     words in the input text.
-
     For example:
-
         >>> make_chains("hi there mary hi there juanita")
         {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
     """
@@ -35,6 +31,7 @@ def make_chains(text_string):
             chains[(words[i], words[i + 1])] = [words[i + 2]]
 
     return chains
+    # print chains
 
 
 
@@ -46,20 +43,16 @@ def make_text(chains):
     text += current_key[0]  + " " + current_key[1] + " "
 
     while True:
-        try:
-            
+            if current_key == ("Sam", "I"):
+                text += chains[current_key][0]
+                break
+                
             chosen_word = choice(chains[current_key])
             text += chosen_word + " "
             new_key = (current_key[1], chosen_word)
             current_key = new_key
-            if current_key == ("Sam", "I"):
-                break
-
-        except:
-           
-            break
-
-    print text
+            
+    return text
 
 
 input_path = "green-eggs.txt"
